6e9b3d5a29c4eef7d8d9895f8e8bf8512bdc7b64
Naci <mkeskin@caltech.edu>
2023-11-27 11:49:28 -0800

    Fixed Kinematic Chain

--- me133a_final/simple_traj.py
+++ me133a_final/simple_traj.py
@@ -22,7 +22,7 @@ from hw5code.TransformHelpers   import *
 from hw5code.TrajectoryUtils    import *
 
 # Grab the general fkin from HW5 P5.
-from hw5code.KinematicChain     import KinematicChain
+from KinematicChain     import KinematicChain
 
 # list the joints
 joint_list = ['back_bkz', 'back_bky', 'back_bkx',
